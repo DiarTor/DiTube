fd0779f4caad601e9b2717e0dd2800d97d6367e8
DiarTor <mhmmd2020.15@gmail.com>
2023-09-04 03:13:54 +0330

    Remove ignored files

--- test.py
+++
@@ -1,6 +0,0 @@
-from pytube import YouTube
-
-link = input("Enter the link: ")
-yt = YouTube(link)
-res = yt.streams.filter(progressive=True).order_by("resolution").last()
-res.download(output_path="videos", filename="test.mp4")
