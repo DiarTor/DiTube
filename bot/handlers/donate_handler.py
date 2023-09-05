8f6351a6cb08cce87854629577480e0a99ea9ffd
DiarTor <mhmmd2020.15@gmail.com>
2023-09-06 03:15:27 +0330

    disable_web_page_view in donate_handler.py

--- bot/handlers/donate_handler.py
+++ bot/handlers/donate_handler.py
@@ -8,6 +8,6 @@ from langs import persian, english
 async def donate(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
     if users_collection.find_one({"user_id": user.id})['lang'] == "fa":
-        await update.message.reply_text(persian.donate, parse_mode='markdown')
+        await update.message.reply_text(persian.donate, parse_mode='markdown', disable_web_page_preview=True)
     elif users_collection.find_one({"user_id": user.id})['lang'] == "en":
-        await update.message.reply_text(english.donate, parse_mode='markdown')
+        await update.message.reply_text(english.donate, parse_mode='markdown', disable_web_page_preview=True)
