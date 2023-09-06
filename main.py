cb0edabb79526e6add8a9ee82416dbba35503207
DiarTor <mhmmd2020.15@gmail.com>
2023-09-07 01:06:37 +0330

    returned logger

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import logging
+
 from bot import bot
+
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     bot.main()
