0e4674643a6221dc8ead9857848e6fa79d71e28d
DiarTor <mhmmd2020.15@gmail.com>
2023-09-12 17:01:08 +0330

    Download Audio Functionality

--- bot/download_videos/send_video.py
+++ bot/download_videos/send_video.py
@@ -11,12 +11,24 @@ async def send(update: Update, yt, chat_id, video_path) -> None:
     description = yt.description[:850] if yt.description else ""
     published = yt.publish_date.strftime("%d/%m/%Y")
     if user_lang == "en":
-        keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Creator YT Channel", url=channel_url)]])
-        await update.effective_chat.send_video(video=video_path,
-                                               caption=f"{yt.title}\n\n👀 Views: {views}\n📝 Description:\n{description}\n\n📅 Publish Date: {published}",
-                                               reply_markup=keyboard)
+        if "mp4" in video_path:
+            keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Creator YT Channel", url=channel_url)]])
+            await update.effective_chat.send_video(video=video_path,
+                                                   caption=f"{yt.title}\n\n👀 Views: {views}\n📝 Description:\n{description}\n\n📅 Publish Date: {published}",
+                                                   reply_markup=keyboard)
+        else:
+            keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Creator YT Channel", url=channel_url)]])
+            await update.effective_chat.send_audio(audio=video_path,
+                                                   caption=f"{yt.title}\n\n👀 Views: {views}\n📝 Description:\n{description}\n\n📅 Publish Date: {published}",
+                                                   reply_markup=keyboard)
     else:
-        keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("کانال یوتیوب سازنده", url=channel_url)]])
-        await update.effective_chat.send_video(video=video_path,
-                                               caption=f"{yt.title}\n\n👀 بازدید: {views}\n📝 توضیحات:\n{description}\n\n📅 تاریخ انتشار: {published}",
-                                               reply_markup=keyboard)
+        if "mp4" in video_path:
+            keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Creator YT Channel", url=channel_url)]])
+            await update.effective_chat.send_video(video=video_path,
+                                                   caption=f"{yt.title}\n\n👀 Views: {views}\n📝 Description:\n{description}\n\n📅 Publish Date: {published}",
+                                                   reply_markup=keyboard)
+        else:
+            keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Creator YT Channel", url=channel_url)]])
+            await update.effective_chat.send_audio(audio=video_path,
+                                                   caption=f"{yt.title}\n\n👀 Views: {views}\n📝 Description:\n{description}\n\n📅 Publish Date: {published}",
+                                                   reply_markup=keyboard)
