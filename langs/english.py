3d072ea0920adff82e802c0ef102d6712855aaef
DiarTor <mhmmd2020.15@gmail.com>
2023-09-15 15:15:19 +0330

    Added more sentences in english.py and persian.py

--- langs/english.py
+++ langs/english.py
@@ -35,10 +35,20 @@ Bot Commands:
 
 donate = "You can support the developer by using one of the links below. 😊\n\nGuide :\nCoffeete for Rial donations\nTron for Digital currency donations\n\nCoffeete : https://www.coffeete.ir/DiarTor\nTron (TRX) : `TJXeM5puLgWu8sHu96riZrrpxLxanvUB59`"
 
-select_quality = "❓Choose The Quality :"
+select_quality = "❓Please Choose one of the buttons bellow :"
 
 timed_out = "❌Timed Out!"
 
 get_video_info = "🔍Getting Video Information..."
 
 age_restricted = "Sorry, the video you want to download is age restricted by youtube so we are not able to download it. 🧐"
+send_user_id_usage = "Usage: /send <user_id>"
+user_not_found = "User not found ❌"
+please_send_message = "Please send the message you want to send"
+message_sent_to_all = "Your message has been sent to all users ✅"
+message_sent_to_user = "Your message has been sent to the user ✅"
+user_not_specified = "User not specified. Please use /send <user_id> first."
+sending_to_all_prompt = "Please send your message to all users"
+no_access = "You don't have access to this command ❌"
+image_sent_to_all = "Your image has been sent to all users ✅"
+image_sent_to_user = "Your image has been sent to all users ✅"
