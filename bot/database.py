2ae8caef3e9f1eedf6aaf3b32c3daf7d6cd8529b
DiarTor <mhmmd2020.15@gmail.com>
2023-09-15 15:58:38 +0330

    Changed database.py

--- bot/database.py
+++ bot/database.py
@@ -3,6 +3,6 @@ import os
 
 from pymongo import MongoClient
 
-client = MongoClient(os.environ.get(MONGODB_URI, "mongodb://localhost:27017/"))
+client = MongoClient("mongodb://localhost:27017/")
 db = client['mitube']
 users_collection = db['users']
