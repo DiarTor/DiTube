327268b4ab46f94b3cbdbfc6ad77f63a6a743620
DiarTor <mhmmd2020.15@gmail.com>
2023-10-05 22:42:13 +0330

    Fixed import issues

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,5 @@
 from bot.config import TOKEN
-from bot.handlers import start_handler, staff_handler, help_handler, message_handler, callback_handler
+from bot.handlers import start_handler, staff_handler, message_handler, callback_handler
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackQueryHandler
 
@@ -10,7 +10,6 @@ def main() -> None:
     application.add_handler(CommandHandler("start", start_handler.start))
     application.add_handler(CommandHandler("send", staff_handler.send))
     application.add_handler(CommandHandler("sendall", staff_handler.sendall))
-    application.add_handler(CommandHandler("adminhelp", help_handler.adminhelp))
     application.add_handler((CallbackQueryHandler(callback_handler.handle_callback)))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler.handle_user_message))
 
