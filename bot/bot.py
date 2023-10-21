5e76ded25402bcf03fbca0fb35c4ff6c99ddecf7
DiarTor <mhmmd2020.15@gmail.com>
2023-10-21 14:50:30 +0330

    removed staff_handler.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,5 @@
 from bot.config import TOKEN
-from bot.handlers import start_handler, staff_handler, message_handler, callback_handler
+from bot.handlers import start_handler, message_handler, callback_handler
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackQueryHandler
 
@@ -8,8 +8,6 @@ def main() -> None:
     application = Application.builder().token(TOKEN).build()
 
     application.add_handler(CommandHandler("start", start_handler.start))
-    application.add_handler(CommandHandler("send", staff_handler.send))
-    application.add_handler(CommandHandler("sendall", staff_handler.sendall))
     application.add_handler((CallbackQueryHandler(callback_handler.handle_callback)))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler.handle_user_message))
 
