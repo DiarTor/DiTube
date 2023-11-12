d5d735ac822598fdfddea9b070a2cbcd33226520
DiarTor <mhmmd2020.15@gmail.com>
2023-11-12 13:04:06 +0330

    Classify buttons.py and get_user_data.py

--- utils/buttons.py
+++ utils/buttons.py
@@ -1,81 +1,138 @@
-from telebot.types import KeyboardButton, InlineKeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup
-from utils.get_user_data import get_user_lang
+from telebot.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup
+from utils.get_user_data import UserDataProvider
 
 
-def homepage_buttons(user_id):
-    user_lang = get_user_lang(user_id)
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    if user_lang == "en":
-        buttons = [[KeyboardButton("🛒 Buy Subscription")],
-                   [KeyboardButton("📋 My Subscription"), KeyboardButton("👤 Account")],
-                   [KeyboardButton("⚙️ Settings"), KeyboardButton("🎁 Gift Code")],
-                   [KeyboardButton("📞 Support"), KeyboardButton("📖 Guide")]]
-    else:
-        buttons = [[KeyboardButton("🛒 خرید اشتراک")], [KeyboardButton("📋 اشتراک من"), KeyboardButton("👤 حساب کاربری")],
-                   [KeyboardButton("⚙️ تنظیمات"), KeyboardButton("🎁 کد هدیه")],
-                   [KeyboardButton("📞 پشتیبانی"), KeyboardButton("📖 راهنما")]]
-    for row in buttons:
-        markup.row(*row)
-    return markup
+class KeyboardMarkupGenerator:
+    def __init__(self, user_id):
+        """
+        initialize class with user_id
+        :param user_id:
+        The unique identifier of the user.
+        """
 
+        self.user_id = user_id
+        self.user_language = UserDataProvider(user_id).get_user_language()
 
-def settings_buttons(user_id):
-    user_lang = get_user_lang(user_id)
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    if user_lang == "en":
-        buttons = [[KeyboardButton("🌐 Change Language")], [KeyboardButton("↩️ Return")]]
-    else:
-        buttons = [[KeyboardButton("🌐 تغییر زبان")], [KeyboardButton("↩️ بازگشت")]]
-    for row in buttons:
-        markup.row(*row)
-    return markup
+    def _create_reply_keyboard(self, buttons):
+        """
+        Create ReplyKeyboardMarkup from list of buttons
+        :param buttons:
+        list of buttons (KeyboardButton)
+        :return:
+        ReplyKeyboardMarkup object
+        """
 
+        markup = ReplyKeyboardMarkup(resize_keyboard=True)
+        for row in buttons:
+            markup.row(*row)
+        return markup
 
-def account_buttons(user_id):
-    user_lang = get_user_lang(user_id)
-    markup = InlineKeyboardMarkup()
-    if get_user_lang(user_id) == "en":
-        buttons = [InlineKeyboardButton(text="💳 Charge Account", callback_data="charge_account"),
-                   InlineKeyboardButton(text="📢 Invite Referrals", callback_data="invite_referrals")]
-    else:
-        buttons = [InlineKeyboardButton(text="💳 شارژ حساب", callback_data="charge_account"),
-                   InlineKeyboardButton(text="📢 زیر مجموعه گیری", callback_data="invite_referrals")]
-    markup.row(*buttons)
-    return markup
+    def _create_inline_keyboard(self, buttons):
+        """
+        Create InlineKeyboardMarkup from list of buttons
+        :param buttons:
+        list of buttons (InlineKeyboardButton)
+        :return:
+        InlineKeyboardMarkup object
+        """
 
+        markup = InlineKeyboardMarkup()
+        for row in buttons:
+            markup.row(*row)
+        return markup
 
-def my_subscription_buttons(user_id):
-    user_lang = get_user_lang(user_id)
-    markup = InlineKeyboardMarkup()
-    if get_user_lang(user_id) == "en":
-        buttons = [InlineKeyboardButton(text="❌ Auto Renew", callback_data="auto_renew")]
-    else:
-        buttons = [InlineKeyboardButton(text="❌ تمدید خودکار", callback_data="auto_renew")]
-    markup.row(*buttons)
-    return markup
+    def homepage_buttons(self):
+        """
+        Create homepage buttons based on user language
+        :return:
+        list of buttons (KeyboardButton)
+        """
 
+        user_language = self.user_language
+        if user_language == 'en':
+            buttons = [[KeyboardButton("🛒 Buy Subscription")],
+                       [KeyboardButton("📋 My Subscription"), KeyboardButton("👤 Account")],
+                       [KeyboardButton("⚙️ Settings"), KeyboardButton("🎁 Gift Code")],
+                       [KeyboardButton("📞 Support"), KeyboardButton("📖 Guide")]]
+        else:
+            buttons = [[KeyboardButton("🛒 خرید اشتراک")],
+                       [KeyboardButton("📋 اشتراک من"), KeyboardButton("👤 حساب کاربری")],
+                       [KeyboardButton("⚙️ تنظیمات"), KeyboardButton("🎁 کد هدیه")],
+                       [KeyboardButton("📞 پشتیبانی"), KeyboardButton("📖 راهنما")]]
+        return self._create_reply_keyboard(buttons)
 
-def subscribe_to_channel_buttons(user_id):
-    user_lang = get_user_lang(user_id)
-    markup = InlineKeyboardMarkup()
-    if get_user_lang(user_id) == "en":
-        buttons = [[InlineKeyboardButton("👉 Join Channel", url="https://t.me/DiarDev")],
-            [InlineKeyboardButton("✅ Joined", url="https://t.me/diartorbot?start=joined")]]
-    else:
-        buttons = [[InlineKeyboardButton("👉 عضویت در کانال", url="https://t.me/DiarDev")],
-            [InlineKeyboardButton("✅ عضو شدم", url="https://t.me/diartorbot?start=joined")]]
+    def return_buttons(self):
+        """
+        Create return button based on user language
+        :return:
+        list of buttons (KeyboardButton)
+        """
 
-    for row in buttons:
-        markup.row(*row)
-    return markup
+        user_language = self.user_language
+        if user_language == 'en':
+            buttons = [KeyboardButton(text="↩️ Return")]
+        else:
+            buttons = [KeyboardButton(text="↩️ بازگشت")]
+        return self._create_reply_keyboard(buttons)
 
+    def settings_buttons(self):
+        """
+        Create settings buttons based on user language
+        :return:
+        list of buttons (KeyboardButton)
+        """
 
-def return_buttons(user_id):
-    user_lang = get_user_lang(user_id)
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    if get_user_lang(user_id) == "en":
-        buttons = [KeyboardButton(text="↩️ Return")]
-    else:
-        buttons = [KeyboardButton(text="↩️ بازگشت")]
-    markup.row(*buttons)
-    return markup
+        user_language = self.user_language
+        if user_language == 'en':
+            buttons = [[KeyboardButton("🌐 Change Language")], [KeyboardButton("↩️ Return")]]
+        else:
+            buttons = [[KeyboardButton("🌐 تغییر زبان")], [KeyboardButton("↩️ بازگشت")]]
+        return self._create_reply_keyboard(buttons)
+
+    def account_buttons(self):
+        """
+        Create account buttons based on user language
+        :return:
+        list of buttons (InlineButton)
+        """
+
+        user_language = self.user_language
+        if user_language == 'en':
+            buttons = [InlineKeyboardButton(text="💳 Charge Account", callback_data="charge_account"),
+                       InlineKeyboardButton(text="📢 Invite Referrals", callback_data="invite_referrals")]
+        else:
+            buttons = [InlineKeyboardButton(text="💳 شارژ حساب", callback_data="charge_account"),
+                       InlineKeyboardButton(text="📢 زیر مجموعه گیری", callback_data="invite_referrals")]
+        return self._create_inline_keyboard(buttons)
+
+    def my_subscription_buttons(self):
+        """
+        Create my subscription buttons based on user language
+        :return:
+        list of buttons (InlineButton)
+        """
+
+        user_language = self.user_language
+        if user_language == 'en':
+            buttons = [InlineKeyboardButton(text="❌ Auto Renew", callback_data="auto_renew")]
+        else:
+            buttons = [InlineKeyboardButton(text="❌ تمدید خودکار", callback_data="auto_renew")]
+        return self._create_inline_keyboard(buttons)
+
+    def subscribe_to_channel_buttons(self):
+        """
+        Create subscribe to channel buttons based on user language
+        :return:
+        list of buttons (InlineButton)
+        """
+
+        user_language = self.user_language
+        channel_link = "https://t.me/DiarDev"
+        starter_link = "https://t.me/diartorbot?start=joined"
+        if user_language == 'en':
+            buttons = [[InlineKeyboardButton("👉 Join Channel", url=channel_link)],
+                       [InlineKeyboardButton("✅ Joined", url=starter_link)]]
+        else:
+            buttons = [[InlineKeyboardButton("👉 عضویت در کانال", url=channel_link)],
+                       [InlineKeyboardButton("✅ عضو شدم", url=starter_link)]]
+        return self._create_inline_keyboard(buttons)
--- utils/get_user_data.py
+++ utils/get_user_data.py
@@ -1,17 +1,75 @@
-import datetime
+import logging
 
 from bot.database import users_collection
-from jdatetime import datetime as jdatetime
 
 
-def get_user_lang(user_id):
-    return users_collection.find_one({"user_id": user_id})["settings"]["language"]
+class UserDataProvider:
+    def __init__(self, user_id):
+        """
+        Initialize UserDataProvider with a user_id.
 
+        :param user_id:
+        The unique identifier for the user.
+        """
 
-def get_user_subscription_data(user_id):
-    return users_collection.find_one({"user_id": user_id})["subscription"]
+        self.user_id = user_id
+        self.user = self._get_user_data()
 
-def get_user_lang_and_return_response(user_id, persian= None, english = None):
-    user_lang = get_user_lang(user_id)
-    response: str = english if user_lang == "en" else persian
-    return response
+    def _get_user_data(self) -> dict:
+        """
+        Retrieve user data from the database.
+
+        Returns :
+        - dict: User data if found, None otherwise.
+        """
+
+        try:
+            user_data = users_collection.find_one({"user_id": self.user_id})
+            return user_data
+        except Exception as e:
+            logging.error(f"Error retrieving user data: {e}")
+            return None
+
+    def get_user_language(self) -> str:
+        """
+        Get the language setting for the user.
+
+        Returns :
+        - not_selected (str): if the user did'nt select a language.
+        - fa (str): if the user selected Farsi(persian).
+        - en (str): if the user selected English.
+        """
+
+        if self.user:
+            return self.user['settings']['language']
+        else:
+            return "not_selected"
+
+    def get_user_subscription_details(self) -> dict:
+        """
+        Get details about the user's subscription.
+
+        Returns :
+        - dict: Subscription details.
+        """
+
+        if self.user:
+            return self.user.get("subscription", {})
+        else:
+            return {}
+
+    def return_response_based_on_language(self, english=None, persian=None) -> str:
+        """
+        Generate a response based on the user's language setting.
+
+        Parameters :
+        - english (str): The response in English.
+        - persian (str): The response in Persian.
+
+        Returns :
+        - str: The response in the user's language.
+        """
+
+        user_language = self.get_user_language()
+        response = english if user_language == "en" else persian
+        return response
