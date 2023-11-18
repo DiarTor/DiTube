208519682f19ba74112f86eb7f9bb1c6d2eaf6cd
DiarTor <mhmmd2020.15@gmail.com>
2023-11-18 19:08:50 +0330

    changed database

--- bot/database.py
+++ bot/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://admin:f6hDyEUr7rY3!ZFsYnwl@188.40.16.3:31419/admin")
+client = MongoClient("mongodb://admin:kR35fSXHkSh$IbiiqO3j@188.40.16.3:30454/admin")
 db = client['ditube']
 users_collection = db['users']
 giftcodes_collection = db['giftcodes']
