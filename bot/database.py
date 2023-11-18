6e28ee8ec8053a2b7a3bf3c7235f36c58f77c63c
DiarTor <mhmmd2020.15@gmail.com>
2023-11-18 18:57:58 +0330

    getting started for deployment

--- bot/database.py
+++ bot/database.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
-# client = MongoClient("mongodb+srv://diartor:VtPLs3RxPsgzs1UT@cluster0.mocauua.mongodb.net/?retryWrites=true&w=majority")
-db = client['mitube']
+client = MongoClient("mongodb://admin:f6hDyEUr7rY3!ZFsYnwl@188.40.16.3:31419/admin")
+db = client['ditube']
 users_collection = db['users']
 giftcodes_collection = db['giftcodes']
