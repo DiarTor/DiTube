98ec0dcf03d8fae1aea4e64c53df4fc4627ec84e
DiarTor <mhmmd2020.15@gmail.com>
2023-11-23 02:43:33 +0330

    changed to local configs

--- config/database.py
+++ config/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://admin:kR35fSXHkSh$IbiiqO3j@188.40.16.3:30454/admin")
+client = MongoClient("mongodb://localhost:27017/")
 db = client['ditube']
 users_collection = db['users']
 giftcodes_collection = db['giftcodes']
