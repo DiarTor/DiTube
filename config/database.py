872de92f685f7a7dc1c555469f558f65c49b1b6e
DiarTor <mhmmd2020.15@gmail.com>
2024-05-28 21:06:58 +0330

    .gitignore is now working

--- config/database.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-# client = MongoClient("mongodb://admin:vnICel577xTiHAR!jXoa@188.40.16.3:30454/admin")
-db = client['ditube']
-users_collection = db['users']
-giftcodes_collection = db['giftcodes']
-factors_collection = db['factors']
-subscriptions_collection = db['subscriptions']
